f482b2dbffa329d87db3f1a0d5e86187331cb7a4
AbhinnS <sayanshabhinn@gmail.com>
2024-10-15 01:30:07 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 103, 58, 183),
       body: Center(
         child: Text('Hello Abhinn!'),
   ),
