64f70c4e082b28a21a5e1d887aebf70a3886b5d5
AbhinnS <sayanshabhinn@gmail.com>
2024-10-16 23:45:28 +0530

    Finished Product

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradiant_container.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 103, 58, 183),
-      body: Center(
-        child: Text('Hello Abhinn!'),
-  ),
+      body: GradientContainer( const Color.fromARGB(255, 69, 38, 124),
+        const Color.fromARGB(255, 43, 2, 90)),
   ),
   ),
 );
